8ee4d54945f2788c6e96b4c3515a5d732014befa
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-03-06 11:35:31 +0600

    first three practice added

---
+++ browser-practice.js
@@ -0,0 +1,18 @@
+//১. setTimeout এর ভিতরে একটা কিছু কনসোল লগ করবে যেটা সাড়ে তিন সেকেন্ড পরে আউটপুট দেখাবে। 
+setTimeout(() => {
+    console.log('output after 3.5 sec')
+}, 3500);
+
+//২. prompt দিয়ে ইউজারের কাছ থেকে একটা সংখ্যা নাও। সেটার সাথে ২০০যোগ করো। যোগ করতে গেলে parse করা লাগলে সেটা করো। তারপর সেই রেজাল্টকে alert দিয়ে আউটপুট দেখাও 
+/* const getInput = prompt('Give any number');
+const inputToNumber = parseFloat(getInput);
+const result = inputToNumber + 200;
+alert(result); */
+
+//৩. ব্রাউজারে একটা confirm করে জিজ্ঞেস করো তুমি কি তোমার ওয়েবসাইট এর লোকেশন দেখতে চাও। যদি বলে দেখতে চাই তাহলে সেই ওয়েবসাইট এর লোকেশন এর href জিনিসটা কনসোল লগ করে দেখাও 
+/* const getOpinion = confirm('do you want to see this website href');
+if(getOpinion){
+    console.log(location.href);
+} */
+
+//৪. Cookies কি জিনিস। এইটা দিয়ে কি করা হয়। সেটা ইংরেজিতে ৫ থেকে ৭ লাইনের মধ্যে লিখে ফেলো। 
